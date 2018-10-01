20434acd9d3651600206b92ef1230518024d0548
Hitesh Tare <hiteshtare@gmail.com>
2018-10-01 20:07:39 +0530

    First Commit

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://trupti:admin123@ds115533.mlab.com:15533/blog_dev',
+  cookieKey: '123123123'
+};
